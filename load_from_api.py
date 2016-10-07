6a7fc2470b746d2fe01ab3f5c154e31943b94641
nidalmer <nidalmeryem@gmail.com>
2016-10-07 02:02:50 +0100

    Update load_from_api.py

--- load_from_api.py
+++ load_from_api.py
@@ -11,75 +11,47 @@ tmdb.API_KEY = '822cba76ea391ade520ab8d8a0038ac9'
 
 # MOVIE LIST
 
-movie_id_list = [342473, 347026]
+movie_id_list = []
 
-# OTHERS
-db_movies = Movie.objects.all()
+movie = tmdb.Movies(movie_id)
+info = movie.info()
+vids = movie.videos()
+credits = movie.credits()
+trailers = vids['results']
 
-db_ids = []
+data, created = Movie.objects.update_or_create(tmdb_id=info['id'], 
+                                               defaults={'title':info['title'],
+                                               'release':info['release_date'],
+                                               'description':info['overview'],
+                                               'backdrop':info['backdrop_path'],
+                                               'poster':info['poster_path'],
+                                               'popularity':info['popularity']})
 
-for db_movie in db_movies:
-    db_ids.append(db_movie.tmdb_id)
+genres = []
+crew = []
+cast = []
+genre_ids = info['genres']
 
-for movie_id in movie_id_list:
+for genre_id in genre_ids:
+    genres.append(genre_id['id'])
 
-    movie = tmdb.Movies(movie_id)
-    info = movie.info()
-    vids = movie.videos()
-    credits = movie.credits()
+for gnre in genres:
+    data.genre.add(gnre)
 
-    data, created = Movie.objects.update_or_create(tmdb_id=info['id'], 
-                                                   defaults={'title':info['title'],
-                                                             'release':info['release_date'],
-                                                             'description':info['overview'],
-                                                             'backdrop':info['backdrop_path'],
-                                                             'poster':info['poster_path'],
-                                                             'popularity':info['popularity']})  
+crew = credits['crew']
+cast = credits['cast']
 
-    if created:
-
-        genres = []
-        crew = []
-        cast = []
-        genre_ids = info['genres']
-
-        for genre_id in genre_ids:
-            genres.append(genre_id)
-
-        for gnre in genres:
-            data.genre.add(gnre)
-
-        trailers = vids['results']
-        for trailer in trailers:
-            trailer, created = Trailer.objects.update_or_create(link=trailer['key'], trailer_title=trailer['name'], movie=data)
-
-        crew = credits['crew']
-        cast = credits['cast']
-
-        for i in range(1, 5):
-            try:
-                actor = next(item for item in cast if item["order"] == i)
-            except:
-                pass
-            act, created = Person.objects.update_or_create(name=actor['name'])
-            data.actors.add(act)
-
-        director = next(item for item in crew if item["department"] == "Directing" and item["job"] == "Director")
-        dir, created = Person.objects.update_or_create(name=director['name'])
-        data.director.add(dir)
-
-    else:
+for i in range(1, 5):
+    try:
+        actor = next(item for item in cast if item["order"] == i)
+    except:
         pass
+    act, created = Person.objects.update_or_create(name=actor['name'])
+    data.actors.add(act)
 
-    trailers = vids['results']
-    for trailer in trailers:
-        trailer, created = Trailer.objects.get_or_create(link=trailer['key'], trailer_title=trailer['name'], movie=db_movie)
+director = next(item for item in crew if item["department"] == "Directing" and item["job"] == "Director")
+dir, created = Person.objects.update_or_create(name=director['name'])
+data.director.add(dir)
 
-
-for db_movie in db_movies:
-    dbmv = tmdb.Movies(db_movie.tmdb_id)
-    info = dbmv.info()
-    vids = dbmv.videos()
-    trailers = vids['results']
-    for trailer in trailers:
-        trailer, created = Trailer.objects.get_or_create(link=trailer['key'], trailer_title=trailer['name'], movie=db_movie)
+for trailer in trailers:
+    trailer, created = Trailer.objects.get_or_create(link=trailer['key'], trailer_title=trailer['name'], movie=data)
